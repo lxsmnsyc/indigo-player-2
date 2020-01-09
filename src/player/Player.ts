ea0a23a56a3bcfafc9869b84ab36eaf78a2a7653
Alexis Munsayac <alexis@lyon.com.ph>
2020-01-09 18:48:45 +0800

    Create Player.ts

---
+++ src/player/Player.ts
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2020 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2020
+ */
+import Module from '../module';
+import { PlayerInterface } from '../types';
+
+export default abstract class Player extends Module implements PlayerInterface {
+  abstract load(): void | Promise<void>;
+
+  abstract unload(): void;
+
+  abstract play(): void;
+
+  abstract pause(): void;
+
+  abstract seekTo(time: number): void;
+
+  abstract setVolume(volume: number): void;
+
+  abstract setSource(src: string): void;
+
+  abstract setPlaybackRate(playbackRate: number): void;
+}
